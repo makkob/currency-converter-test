b8175e0503ed165d90c79d37cb9a922dd9736ab2
Maksym Kobets <94405611+makkob@users.noreply.github.com>
2024-07-11 23:55:45 +0300

    1.0.0
    build 1

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    // додайте інші налаштування середовища, які вам потрібні
-  };
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    // додайте інші налаштування середовища, які вам потрібні
-  };
